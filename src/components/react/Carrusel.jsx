03079181372528288ae968c7f72bb114ce964aaf
Enrique Albert <enalcadev@gmail.com>
2025-05-09 22:18:43 +0200

    fix: clean up carousel component by removing unused imports and improving layout

--- src/components/react/Carrusel.jsx
+++ src/components/react/Carrusel.jsx
@@ -4,9 +4,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
+  CarouselItem
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import { useRef } from "react";
@@ -33,9 +31,17 @@ export default function Carrusel() {
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index}>
-              <Card>
-                <CardContent className="flex items-center justify-center w-full">
-                  <img src={CAROUSEL_ITEMS[index]} alt="Imagen" className="rounded-lg md:h-[800px] xs:w-[800px] md:w-[1200px] object-cover" />
+              <Card className="bg-gray-50">
+                <CardContent className="flex items-center justify-center">
+                  
+                  <img src={CAROUSEL_ITEMS[index]} alt="Imagen" className="rounded-lg max-h-[180px] md:max-h-[400px] object-cover" />
+                  <div className="hidden md:flex md:flex-col  justify-top mx-8 gap-2">
+                    <h1 className="text-5xl lg:text-7xl text-balance text-black font-display uppercase">
+                      Imagen {index + 1}
+                    </h1>                    
+                    <h2 className="text-3xl lg:text-5xl text-balance text-black text-right">895,99 €</h2>
+                    <p className="text-lg lg:text-2xl text-balance text-gray-600 text-right line-through">1.200 €</p>                    
+                  </div>
                 </CardContent>
               </Card>
           </CarouselItem>
