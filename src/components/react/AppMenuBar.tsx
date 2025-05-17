4819f67d2eb462a14eb5729cf12718fe4b713d22
Enrique Albert <enalcadev@gmail.com>
2025-05-17 13:46:26 +0200

    feat: enhance AppMenuBar component with dynamic menu items and scroll functionality

--- src/components/react/AppMenuBar.tsx
+++ src/components/react/AppMenuBar.tsx
@@ -8,25 +8,42 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar"
 
-export default function AppMenuBar() {
+import { Menu } from "lucide-react"
+
+import type { MenuSection } from '@/types/menu';
+
+interface Props {
+    menuItems: MenuSection[][];
+}
+
+export default function AppMenuBar({ menuItems }: Props) {
+
+    const scrollToSection = (sectionTitle: string) => {
+        
+        const element = document.querySelector(`[data-section="${sectionTitle}"]`);
+        if (element) {
+            const elementPosition = element.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.scrollY - 50;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    };
     return (
         <Menubar>
             <MenubarMenu>
-                <MenubarTrigger>Products</MenubarTrigger>
+                <MenubarTrigger>
+                    <Menu className="mr-2 h-4 w-4" />
+                </MenubarTrigger>
                 <MenubarContent>
-                    <MenubarItem>
-                        New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                    </MenubarItem>
-                    <MenubarItem>SOFÁS</MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarItem>SOFÁ CHAISELONGUE</MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarItem>SILLÓN RELAX</MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarItem>DESCANSO</MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarItem>STOCK</MenubarItem>
-                    <MenubarSeparator />
+                    {menuItems.map((section) => (
+                        <MenubarItem key={section[0].title} onClick={() => scrollToSection(section[0].title)}>
+                            {section[0].title}
+                            <MenubarSeparator />
+                        </MenubarItem>                        
+                    ))}
                 </MenubarContent>
             </MenubarMenu>
         </Menubar>
